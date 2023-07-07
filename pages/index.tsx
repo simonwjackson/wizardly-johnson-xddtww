b2faf8723878c50bc0fd496f60bb5c6d5c2b65d7
Simon W. Jackson <hello@simonwjackson.io>
2023-07-06 22:09:46 -0500

    style(index): enhance readability and accessibility of weather search form
    - Added 'htmlFor' attribute to label for accessibility
    - Added 'id' attribute to input for better form control

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,15 +14,19 @@ export default function IndexPage() {
         }}
       >
         <div className="mb-16 flex">
-          <label className="flex block font-medium leading-6 font-medium w-full self-center justify-self-end mr-4">
+          <label
+            className="flex block font-medium leading-6 font-medium w-full self-center justify-self-end mr-4"
+            htmlFor="city"
+          >
             Weather Search:
           </label>
           <input
             data-testid="weather-input"
             type="text"
             name="city"
+            id="city"
             className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-          />{" "}
+          />
           <button
             type="submit"
             className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-white bg-sky-600 uppercase ring-inset"
