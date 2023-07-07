76ef6b00a09ce57ff5cdbea0b9f162cabfaa8d96
Simon W. Jackson <hello@simonwjackson.io>
2023-07-06 22:05:26 -0500

    style(ui): improve form layout and styling
    - Added margin-top to form for better spacing
    - Replaced span with label for accessibility
    - Adjusted layout of city weather display

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,27 +6,34 @@ export default function IndexPage() {
   return (
     <div className="py-2 bg-zinc-100 h-screen">
       <form
-        className="flex items-center justify-center"
+        className="flex items-center justify-center mt-16"
         onSubmit={(e) => {
           e.preventDefault();
           const formdata = new FormData(e.currentTarget);
           setCity(formdata.get("city").toString());
         }}
       >
-        <span>Weather Search:</span>{" "}
-        <input
-          data-testid="weather-input"
-          className="ml-2 border px-2 py-1 border-black"
-          type="text"
-          name="city"
-        />
-        <button className="ml-2 text-sm border rounded-lg p-2" type="submit">
-          Submit
-        </button>
+        <div className="mb-16 flex">
+          <label className="flex block font-medium leading-6 font-medium w-full self-center justify-self-end mr-4">
+            Weather Search:
+          </label>
+          <input
+            data-testid="weather-input"
+            type="text"
+            name="city"
+            className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+          />{" "}
+          <button
+            type="submit"
+            className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-white bg-sky-600 uppercase ring-inset"
+          >
+            Submit
+          </button>
+        </div>
       </form>
 
       {city && (
-        <div className="mt-4 flex items-center justify-center">
+        <div className="flex items-center justify-center">
           <CityWeather city={city} />
         </div>
       )}
