7776a22f504822021f36252386facff609434a8c
Simon W. Jackson <hello@simonwjackson.io>
2023-07-11 10:57:24 -0500

    refactor(weather): add detailed weather response type
    - Added a new type `WeatherResponse` to handle detailed weather data from API
    - Updated the API handler to use the new `WeatherResponse` type for parsing the API response
    - Updated the API response from `res.status(200).json(apiResponse)` to `res.status(200).json(json)` to reflect the use of the new type

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -1,6 +1,54 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import "isomorphic-fetch";
 
+// TODO: This type might not account for every possible return type.
+export type WeatherResponse =
+  | {
+    coord: {
+      lon: number;
+      lat: number;
+    };
+    weather: Array<{
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }>;
+    base: string;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      humidity: number;
+    };
+    visibility: number;
+    wind: {
+      speed: number;
+      deg: number;
+    };
+    clouds: {
+      all: number;
+    };
+    dt: number;
+    sys: {
+      type: number;
+      id: number;
+      country: string;
+      sunrise: number;
+      sunset: number;
+    };
+    timezone: number;
+    id: number;
+    name: string;
+    cod: number;
+  }
+  | {
+    cod: string;
+    message: string;
+  };
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -11,9 +59,10 @@ export default async function handler(
     const { OPENWEATHERMAP_API_KEY } = process.env;
     const apiResponse = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHERMAP_API_KEY}`,
-    ).then((r) => r.json());
+    );
+    const json: WeatherResponse = await apiResponse.json();
 
-    return res.status(200).json(apiResponse);
+    return res.status(200).json(json);
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error" });
   }
