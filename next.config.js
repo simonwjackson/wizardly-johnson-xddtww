6720e8dc94082cf54fea941ae1522a07fd2adc2d
Simon W. Jackson <hello@simonwjackson.io>
2023-07-11 11:21:37 -0500

    feat(next.config): add headers function to next config
    - Added an async headers function to next.config.js
    - Set Access-Control-Allow-Origin header for all API routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
