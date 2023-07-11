b4637e4475de7afc0d782c46b6cee5a5439bc0ad
Simon W. Jackson <hello@simonwjackson.io>
2023-07-10 23:57:12 -0500

    test(city-weather): add city-weather integration tests
    - Updated city-weather-refactor.tsx to include test id for better testing
    - Modified index.test.tsx to include more specific tests
    - Improved error handling in tests
    - Removed commented out code
    - Changed mock server response to handle different cities
    - Updated tests to check for specific city weather information

--- components/city-weather-refactor.tsx
+++ components/city-weather-refactor.tsx
@@ -23,32 +23,40 @@ function CityWeather({ city }: CityWeatherProps) {
 
   return (
     <div aria-busy={isObjectEmpty(weatherResult)}>
-      {(weatherResult?.cod === 200) &&
-          (
-            <div className="p-4 drop-shadow-lg rounded-md bg-white flex flex-col justify-items-center text-gray-400 font-medium">
-              <h1 className="text-center uppercase text-xl font-black text-gray-600">
-                {city}
-              </h1>
-              <Image
-                className="mx-auto"
-                width={OPENWEATHERMAP_2X_SIZE}
-                height={OPENWEATHERMAP_2X_SIZE}
-                alt="weather icon"
-                src={`http://openweathermap.org/img/wn/${weatherResult
-                  ?.weather[0]
-                  ?.icon}@2x.png`}
-              />
-              <div className="text-center capitalize mb-2 font-bold">
-                {weatherResult?.weather[0]?.description}
-              </div>
-              <div className="text-center text-xs">
-                Temperature:
-                <span className="text-3xl text-black ml-2">
-                  {KtoF(weatherResult?.main?.temp)?.toFixed(0)}&#8457;
-                </span>
-              </div>
-            </div>
-          ) || (
+      {(weatherResult?.cod === 200 && (
+        <div className="p-4 drop-shadow-lg rounded-md bg-white flex flex-col justify-items-center text-gray-400 font-medium">
+          <h1
+            className="text-center uppercase text-xl font-black text-gray-600"
+            data-testid="city-weather--name"
+          >
+            {weatherResult.name}
+          </h1>
+          <Image
+            className="mx-auto"
+            width={OPENWEATHERMAP_2X_SIZE}
+            height={OPENWEATHERMAP_2X_SIZE}
+            alt="weather icon"
+            data-testid="city-weather--icon"
+            src={`http://openweathermap.org/img/wn/${weatherResult?.weather[0]
+              ?.icon}@2x.png`}
+          />
+          <div
+            data-testid="city-weather--description"
+            className="text-center capitalize mb-2 font-bold"
+          >
+            {weatherResult?.weather[0]?.description}
+          </div>
+          <div className="text-center text-xs">
+            Temperature:
+            <span
+              className="text-3xl text-black ml-2"
+              data-testid="city-weather--temperature"
+            >
+              {KtoF(weatherResult?.main?.temp)?.toFixed(0)}&#8457;
+            </span>
+          </div>
+        </div>
+      )) || (
         <div className="rounded-md bg-red-50 p-4 drop-shadow-lg">
           <div className="flex">
             <div className="ml-3">
--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,124 +1,129 @@
 import "isomorphic-fetch";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import IndexPage from "./index";
 
 const server = setupServer(
-  rest.get("https://api.openweathermap.org/*", (_req, res, ctx) => {
-    return res(
-      ctx.json({
-        weather: [
-          {
-            description: "Overcast clouds",
-          },
-        ],
-        main: {
-          // temp in Kelvin
-          temp: 295.372,
-        },
-      }),
-    );
-  }),
+  rest.get(
+    "https://api.openweathermap.org/data/2.5/weather",
+    (req, res, ctx) => {
+      const city = req.url.searchParams.get("q");
+
+      let response;
+
+      switch (city) {
+        case "Austin":
+          response = {
+            coord: { lon: -97.7431, lat: 30.2672 },
+            weather: [
+              { id: 800, main: "Clear", description: "clear sky", icon: "01n" },
+            ],
+            base: "stations",
+            main: {
+              temp: 306.79,
+              feels_like: 312.1,
+              temp_min: 305.14,
+              temp_max: 308.09,
+              pressure: 1020,
+              humidity: 54,
+            },
+            visibility: 10000,
+            wind: { speed: 5.66, deg: 160 },
+            clouds: { all: 0 },
+            dt: 1689045255,
+            sys: {
+              type: 2,
+              id: 2003218,
+              country: "US",
+              sunrise: 1688988996,
+              sunset: 1689039337,
+            },
+            timezone: -18000,
+            id: 4671654,
+            name: "Austin",
+            cod: 200,
+          };
+          break;
+        case "Paris":
+          response = {
+            coord: { lon: 2.3488, lat: 48.8534 },
+            weather: [
+              { id: 800, main: "Clear", description: "clear sky", icon: "01d" },
+            ],
+            base: "stations",
+            main: {
+              temp: 290.33,
+              feels_like: 289.98,
+              temp_min: 287.33,
+              temp_max: 292.58,
+              pressure: 1016,
+              humidity: 72,
+            },
+            visibility: 10000,
+            wind: { speed: 1.54, deg: 200 },
+            clouds: { all: 0 },
+            dt: 1689049787,
+            sys: {
+              type: 1,
+              id: 6550,
+              country: "FR",
+              sunrise: 1689047915,
+              sunset: 1689105185,
+            },
+            timezone: 7200,
+            id: 2988507,
+            name: "Paris",
+            cod: 200,
+          };
+
+          break;
+        default:
+          response = {
+            cod: 404,
+            message: "city not found",
+          };
+      }
+
+      return res(ctx.json(response));
+    },
+  ),
 );
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test("it shows weather results", async () => {
+test("it shows weather results, based on user query", async () => {
   render(<IndexPage />);
-  // todo: write some assertions, https://testing-library.com/docs/react-testing-library/example-intro
+  const input = screen.getByTestId("weather-input");
+
+  const query = async (city: string, temp: number, description: string) => {
+    await userEvent.clear(input);
+    await userEvent.type(input, city);
+    expect(input).toHaveValue(city);
+    userEvent.click(screen.getByText(/submit/i));
+
+    await waitFor(() => {
+      screen.getByTestId("city-weather");
+      expect(screen.getByTestId("city-weather--name")).toHaveTextContent(city);
+      expect(screen.getByTestId("city-weather--temperature")).toHaveTextContent(
+        temp.toString(),
+      );
+      expect(screen.getByTestId("city-weather--description")).toHaveTextContent(
+        description,
+      );
+      expect(
+        screen
+          .getByTestId("city-weather--icon")
+          ?.getAttribute("src")
+          ?.startsWith("/_next/image?url="),
+      ).toBe(true);
+    });
+  };
+
+  await query("Austin", 93, "clear sky");
+  await query("Paris", 63, "clear sky");
 });
-
-// todo: add more tests, maybe error handling?
-// BUG: isomorphic-unfetch appears to be causing a segfault when running tests
-// error: Command failed with signal "SIGSEGV".
-// I can get around this by using node 18+ and adding globals to the jest config
-// However, with this approach, we loose the ability to use msw for mocking
-// Unfortunately, this means we will need to use real network requests to run these tests.
-// Before moving forward with any other features, we should address this bug.
-
-// import { rest } from "msw";
-// import { setupServer } from "msw/node";
-// import { render, screen, waitFor } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-//
-// import IndexPage from "./index";
-//
-// // const server = setupServer(
-// //   rest.get("https://api.openweathermap.org/data/**", (_req, res, ctx) => {
-// //     return res(
-// //       ctx.json({
-// //         weather: [
-// //           {
-// //             description: "Overcast clouds",
-// //           },
-// //         ],
-// //         main: {
-// //           // temp in Kelvin
-// //           temp: 295.372,
-// //         },
-// //       }),
-// //     );
-// //   }),
-// // );
-// //
-// // beforeAll(() => server.listen());
-// // afterEach(() => server.resetHandlers());
-// // afterAll(() => server.close());
-//
-// test("it shows weather results", async () => {
-//   render(<IndexPage />);
-//   const input = screen.getByTestId("weather-input");
-//
-//   await userEvent.type(input, "New York");
-//   expect(input).toHaveValue("New York");
-//   userEvent.click(screen.getByText(/submit/i));
-//
-//   await waitFor(() => {
-//     screen.getByTestId("city-weather");
-//   });
-// });
-//
-// test("it handles 404s", async () => {
-//   render(<IndexPage />);
-//   const input = screen.getByTestId("weather-input");
-//
-//   await userEvent.type(input, "London, TX");
-//   userEvent.click(screen.getByText(/submit/i));
-//
-//   await waitFor(() => {
-//     screen.getByText(/city not found/i);
-//   });
-// });
-//
-// // test("it handles a network error", async () => {
-// //   server.use(
-// //     rest.get("https://api.openweathermap.org/*", (_req, res, ctx) => {
-// //       return res(ctx.status(500));
-// //     }),
-// //   );
-// //   render(<IndexPage />);
-// //
-// //   userEvent.type(screen.getByTestId("weather-input"), "New York");
-// //   userEvent.click(screen.getByText(/submit/i));
-// //
-// //   await screen.findByText(/something went wrong/i);
-// // });
-//
-// // test("it handles a 404 error", async () => {
-// //   server.use(
-// //     rest.get("https://api.openweathermap.org/*", (_req, res, ctx) => {
-// //       return res(ctx.json({ "cod": "404", "message": "city not found" }));
-// //     }),
-// //   );
-// //   render(<IndexPage />);
-// //
-// //   userEvent.type(screen.getByTestId("weather-input"), "London, TX");
-// //   userEvent.click(screen.getByText(/submit/i));
-// //
-// //   await screen.findByText(/city not found/i);
-// // });
