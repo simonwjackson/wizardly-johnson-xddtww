83d7331f35a02b45b4c0ace8080665a4406c1f0b
Simon W. Jackson <hello@simonwjackson.io>
2023-07-11 19:30:35 +0000

    refactor(api): remove unused code and update API key retrieval

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -4,61 +4,63 @@ import "isomorphic-fetch";
 // TODO: This type might not account for every possible return type.
 export type WeatherResponse =
   | {
-    coord: {
-      lon: number;
-      lat: number;
-    };
-    weather: Array<{
+      coord: {
+        lon: number;
+        lat: number;
+      };
+      weather: Array<{
+        id: number;
+        main: string;
+        description: string;
+        icon: string;
+      }>;
+      base: string;
+      main: {
+        temp: number;
+        feels_like: number;
+        temp_min: number;
+        temp_max: number;
+        pressure: number;
+        humidity: number;
+      };
+      visibility: number;
+      wind: {
+        speed: number;
+        deg: number;
+      };
+      clouds: {
+        all: number;
+      };
+      dt: number;
+      sys: {
+        type: number;
+        id: number;
+        country: string;
+        sunrise: number;
+        sunset: number;
+      };
+      timezone: number;
       id: number;
-      main: string;
-      description: string;
-      icon: string;
-    }>;
-    base: string;
-    main: {
-      temp: number;
-      feels_like: number;
-      temp_min: number;
-      temp_max: number;
-      pressure: number;
-      humidity: number;
-    };
-    visibility: number;
-    wind: {
-      speed: number;
-      deg: number;
-    };
-    clouds: {
-      all: number;
-    };
-    dt: number;
-    sys: {
-      type: number;
-      id: number;
-      country: string;
-      sunrise: number;
-      sunset: number;
-    };
-    timezone: number;
-    id: number;
-    name: string;
-    cod: number;
-  }
+      name: string;
+      cod: number;
+    }
   | {
-    cod: string;
-    message: string;
-  };
+      cod: string;
+      message: string;
+    };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
   const { q: city } = req.query;
 
   try {
-    const { OPENWEATHERMAP_API_KEY } = process.env;
+    // TODO: use process.env in the near future
+    // const { OPENWEATHERMAP_API_KEY } = process.env;
+    const OPENWEATHERMAP_API_KEY = "a08377ee14fac6dc7f67ade10d26d8ec";
     const apiResponse = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHERMAP_API_KEY}`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHERMAP_API_KEY}`
     );
     const json: WeatherResponse = await apiResponse.json();
 
